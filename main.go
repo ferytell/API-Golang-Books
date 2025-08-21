5e237134b5008aef729f86f3755a6da676413759
ferytell <darkstar2709@gmail.com>
2025-08-21 16:33:17 +0000

    add port on main

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"API-Books/initializer"
 	"API-Books/routers"
+	"log"
+	"os"
 )
 
 func init() {
@@ -12,5 +14,17 @@ func init() {
 }
 
 func main() {
-	routers.StartServer().Run()
+	r := routers.StartServer()
+	// routers.StartServer().Run()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // fallback for local dev
+	}
+
+	log.Printf("Starting server on port %s...\n", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatal("Failed to run server: ", err)
+	}
+
 }
