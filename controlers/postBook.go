4586605c8b98f6e560da971ef8b35f00cb042c1f
ferytell <darkstar2709@gmail.com>
2025-08-24 12:18:45 +0000

    deploy fixx

--- controlers/postBook.go
+++ controlers/postBook.go
@@ -40,17 +40,4 @@ func CreateBook(ctx *gin.Context) {
 		"post": post,
 	})
 
-	// var newBook Book
-
-	// if err := ctx.ShouldBindJSON(&newBook); err != nil {
-	// 	ctx.AbortWithError(http.StatusBadRequest, err)
-	// 	return
-	// }
-
-	// newBook.BookId = fmt.Sprintf("c%d", len(BookDatas)+1)
-	// BookDatas = append(BookDatas, newBook)
-
-	// ctx.JSON(http.StatusCreated, gin.H{
-	// 	"book": newBook,
-	// })
 }
