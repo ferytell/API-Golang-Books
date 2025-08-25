72f38aba871a4e42e6661b069bd1b03c599d094f
ferytell <darkstar2709@gmail.com>
2025-08-25 16:14:23 +0000

    add cors

--- routers/Routers.go
+++ routers/Routers.go
@@ -11,14 +11,24 @@ import (
 func StartServer() *gin.Engine {
 	router := gin.Default()
 
-	// Enable CORS
-	router.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Next()
-	})
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://localhost:3000"}
+	config := cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://ferytell.github.io"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		AllowCredentials: true,
+	}
 	router.Use(cors.New(config))
+
+
+	// Enable CORS
+	// router.Use(func(c *gin.Context) {
+	// 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// 	c.Next()
+	// })
+	// config := cors.DefaultConfig()
+	// config.AllowOrigins = []string{"http://localhost:3000", "https://ferytell.github.io"}
+
+	// router.Use(cors.New(config))
 
 	router.POST("/api/signup", controlers.SignUp)
 	router.POST("/api/login", controlers.Login)
